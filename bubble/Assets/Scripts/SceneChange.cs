2306ab974e494835a1fdcd723de9fc8dd47f4c3a
YangWooSong <olivia6844@naver.com>
2023-01-07 14:38:11 +0900

    23_01_7
    Start, Menu 씬 추가 및 Map정리

---
+++ bubble/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+   public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void To_01_Map()
+    {
+        SceneManager.LoadScene("01_Map");
+    }
+    public void To_02_Map()
+    {
+        SceneManager.LoadScene("02_Map");
+    }
+    public void To_03_Map()
+    {
+        SceneManager.LoadScene("03_Map");
+    }
+}
