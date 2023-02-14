4c1fdcea8675f8e29039f2632993c8f1ab40a734
chan hong <106529266+sdfnkj@users.noreply.github.com>
2023-02-14 11:55:38 +0900

    2023.2.14
    add joystick, camara move, two script
    please yangwoosong add animation I'm going japan good bye

---
+++ bubble/Assets/Scripts/Joystick.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; // 키보드, 마우스, 터치를 이벤트로 오브젝트에 보낼 수 있는 기능을 지원
+
+public class Joystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    private RectTransform lever;
+    private RectTransform rectTransform;
+    [SerializeField, Range(10f, 150f)]
+    private float leverRange;
+
+    private Vector2 inputDirection;    // 추가
+    private bool isInput;    // 추가
+    [SerializeField]
+    private TPSCharacterController controller;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        ControlJoystickLever(eventData);  // 추가
+        isInput = true;    // 추가
+    } 
+    public void OnDrag(PointerEventData eventData)
+    {
+        ControlJoystickLever(eventData);    // 추가
+        
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        lever.anchoredPosition = Vector2.zero;
+        isInput = false;
+        controller.Move(Vector2.zero);
+    }
+    public void ControlJoystickLever(PointerEventData eventData)
+    {
+        var inputDir = eventData.position - rectTransform.anchoredPosition;
+        var inputVector = inputDir.magnitude < leverRange ? inputDir
+            : inputDir.normalized * leverRange;
+        lever.anchoredPosition = inputVector;
+        inputDirection = inputVector / leverRange;
+    }
+    private void InputControlVector()
+    {
+        controller.Move(inputDirection);
+    }
+    
+    
+}
